c10afeb26bd25d57ebe20d34b81535aa2c9e7f6b
DeVector <diniz.vito@gmail.com>
2022-02-24 15:47:44 -0300

    Adicionando controllers e services da aplicacao

---
+++ src/main/java/com/victor/helpdesk/service/TecnicoService.java
@@ -0,0 +1,20 @@
+package com.victor.helpdesk.service;
+
+import com.victor.helpdesk.domain.Tecnico;
+import com.victor.helpdesk.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
