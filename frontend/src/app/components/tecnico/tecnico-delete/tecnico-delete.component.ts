fdc1f53a90c47c5e51ae85133474fbd1486bbf21
DeVector <diniz.vito@gmail.com>
2022-11-29 16:25:53 -0300

    Tecnico-delete finalizado

--- frontend/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ frontend/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { Tecnico } from '../../entities/Tecnico';
 
 @Component({
   selector: 'app-tecnico-delete',
   templateUrl: './tecnico-delete.component.html',
   styleUrls: ['./tecnico-delete.component.css']
 })
+
 export class TecnicoDeleteComponent implements OnInit {
 
-  constructor() { }
+  tecnico: Tecnico ={
+    id:          '',
+    name:        '',
+    cpf:         '',
+    email:       '',
+    password:    '',
+    perfis:      [],
+    localDate:   ''
+  }
 
-  ngOnInit(): void {
+  constructor(
+    private service: TecnicoService,
+    private toastr:  ToastrService,
+    private router:  Router,
+    private route: ActivatedRoute,
+    ) { }
+
+  ngOnInit(): void { 
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe(resposta => {
+      resposta.perfis = [];
+      this.tecnico = resposta;
+    })
+  }
+  
+  delete(): void {
+    this.service.delete(this.tecnico.id).subscribe(() => {
+      this.toastr.success('Técnico deletado com sucesso', 'Delete');
+      this.router.navigate(['tecnicos'])
+    }, ex => {
+      if(ex.error.list) {
+        ex.error.list.forEach( element => {
+          this.toastr.error(element.fildName);
+        });
+      } else {
+        this.toastr.error(ex.error.message);
+      }
+    })
   }
 
 }
+
