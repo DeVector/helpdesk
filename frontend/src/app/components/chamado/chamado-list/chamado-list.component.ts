780b48d1a94dd6d423d6f6077f4c9ba7015d2121
DeVector <diniz.vito@gmail.com>
2022-11-29 18:06:36 -0300

    Filtro e findAll feito

--- frontend/src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ frontend/src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ChamadoService } from 'src/app/services/chamado.service';
 import { Chamados } from '../../entities/Chamados';
 
 @Component({
@@ -10,34 +11,65 @@ import { Chamados } from '../../entities/Chamados';
 })
 export class ChamadoListComponent implements OnInit {
 
-  ELEMENT_DATA: Chamados[] = [
-    {
-      id: 1,
-      dtAbertura: '01/11/2022',
-      dtFechamento: '29/11/2022',
-      prioridade: 'ALTA',
-      status: 'ANDAMENTO',
-      titulo: 'Chamado 1',
-      observacao: 'Testando chamado 1',
-      tecnico: 1,
-      cliente: 7,
-      nameTecnico: 'Luiz Bezerra',
-      nameCliente: 'Wanessa Ribeiro',
-    }
-  ];
+  ELEMENT_DATA: Chamados[] = [];
+  FILTERED_DATA: Chamados[] = [];
 
   displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dtAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Chamados>(this.ELEMENT_DATA);
-
-  constructor() { }
-
-  ngOnInit(): void { }
-
+  
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: ChamadoService
+  ) { }
+
+  ngOnInit(): void { 
+    this.findAll();
+   }
+
+  findAll(): void {
+    this.service.findAll().subscribe( resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamados>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  returnStatus(status: any){
+    if (status == '0'){
+      return 'ABERTO';
+    } else if (status == '1') {
+      return 'ANDAMENTO';
+    } else {
+      return 'ENCERRADO';
+    }
+  }
+
+  returnPrioridade(prioridade: any) {
+    if( prioridade == '0'){
+      return 'ALTA';
+    } else if (prioridade == '1'){
+      return 'MEDIA';
+    } else {
+      return 'BAIXA';
+    }
+  }
+
+  orderByStatus(status: any): void {
+    let list: Chamados[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status == status) {
+        list.push(element);
+      }
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamados>(this.FILTERED_DATA);
+      this.dataSource.paginator = this.paginator;
   }
 
 }
---
+++ frontend/src/app/services/chamado.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Chamados } from '../components/entities/Chamados';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  findAll(): Observable<Chamados[]> {
+    return this.http.get<Chamados[]>(`${API_CONFIG.baseUrl}/chamados`);
+  }
+}
