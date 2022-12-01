17a6ded98e39838135064478560d38d642fa08f5
DeVector <diniz.vito@gmail.com>
2022-12-01 12:03:07 -0300

    HTML e CSS para chamado-read concluido

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 //Component do projeto
 import { NavComponent } from './components/nav/nav.component';
@@ -89,6 +90,7 @@ import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-
     MatIconModule,
     MatListModule,
     MatCardModule,
+    MatDatepickerModule,
     ToastrModule.forRoot({
       timeOut: 4000,
       closeButton: true,
--- frontend/src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ frontend/src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { Chamados } from '../../entities/Chamados';
 
 @Component({
   selector: 'app-chamado-read',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoReadComponent implements OnInit {
 
-  constructor() { }
+  chamado: Chamados = {
+    prioridade:   '',
+    status:       '',
+    titulo:       '',
+    observacao:   '',
+    tecnico:      '',
+    cliente:      '',
+    nameCliente:  '',
+    nameTecnico:  '',
+    dtAbertura:   '',
+    dtFechamento: '',
+  }
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private toastrService: ToastrService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+   findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.toastrService.success('Chamado encontrado', 'Detalhes do chamado');
+      this.chamado = resposta;
+    }, ex => {
+      console.log(ex);
+    })
+   }
+
+  returnStatus(status: any){
+    if (status == '0'){
+      return 'ABERTO';
+    } else if (status == '1') {
+      return 'ANDAMENTO';
+    } else {
+      return 'ENCERRADO';
+    }
+  }
+
+  returnPrioridade(prioridade: any) {
+    if( prioridade == '0'){
+      return 'ALTA';
+    } else if (prioridade == '1'){
+      return 'MEDIA';
+    } else {
+      return 'BAIXA';
+    }
   }
 
 }
